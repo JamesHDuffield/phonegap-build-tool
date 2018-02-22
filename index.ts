18c7a4c202c4a81897ca2cae4f4efb0c127ff5f3
Frank Fenton <frank.fenton@fusion5.com.au>
2018-02-22 18:43:35 +1100

    really fixed it this time

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
-import * as request from 'request-promise';
-import * as fs from 'fs';
-import * as _ from 'lodash';
-import * as args from 'commander';
-import * as archiver from 'archiver';
-import * as streamBuffers from 'stream-buffers';
+import * as archiver from 'archiver'
+import * as args from 'commander'
+import * as fs from 'fs'
+import * as _ from 'lodash'
+import * as request from 'request-promise'
+import * as streamBuffers from 'stream-buffers'
 
-const baseUrl = 'https://build.phonegap.com/api/v1';
-const pollTime = 10000;
+const baseUrl = 'https://build.phonegap.com/api/v1'
+const pollTime = 10000
 
 args
   .version('0.1')
@@ -17,65 +17,65 @@ args
   .option('-p, --platform <item>', 'Platform')
   .option('-k, --keystorePassword <item>', 'Keystore password')
   .option('-s, --keyPassword <item>', 'Signing key password')
-  .parse(process.argv);
+  .parse(process.argv)
 
 async function sleep(duration: number) {
   return new Promise((resolve, reject) => {
-    setTimeout(() => resolve(), duration);
-  });
+    setTimeout(() => resolve(), duration)
+  })
 }
 
 async function zip(): Promise<Buffer> {
   const fileOutput = new streamBuffers.WritableStreamBuffer()
   const archive = archiver('zip', {
     zlib: { level: 9 },
-  });
-  archive.pipe(fileOutput);
-  archive.glob('www/**/*');
-  archive.glob('resources/**/*');
-  archive.glob('config.xml');
-  await archive.finalize();
-  console.log('Zipped app for upload');
-  return fileOutput.getContents();
+  })
+  archive.pipe(fileOutput)
+  archive.glob('www/**/*')
+  archive.glob('resources/**/*')
+  archive.glob('config.xml')
+  await archive.finalize()
+  console.log('Zipped app for upload')
+  return fileOutput.getContents()
 }
 
 async function build(platform: string) {
-  const zippedApp = await zip();
+  const zippedApp = await zip()
   // Get all keys
-  const response = await request.get(`${baseUrl}/keys?auth_token=${args.token}`);
-  const keyId = _.get(JSON.parse(response), `keys.${platform}.all[0].id`);
+  const response = await request.get(`${baseUrl}/keys?auth_token=${args.token}`)
+  const keyId = _.get(JSON.parse(response), `keys.${platform}.all[0].id`)
   if (!keyId) {
-    console.info('No signing key found for this platform');
+    console.info('No signing key found for this platform')
   } else {
      // Unlock key
-    const password = platform === 'ios' ? { password: args.keystorePassword } : { key_pw: args.keyPassword, keystore_pw: args.keystorePassword };
-    await request.put(`${baseUrl}/keys/${platform}/${keyId}?auth_token=${args.token}`, { formData: { data: JSON.stringify(password) } });
-    console.log('Unlocked key');
+    const password = platform === 'ios' ? { password: args.keystorePassword } : { key_pw: args.keyPassword, keystore_pw: args.keystorePassword }
+    await request.put(`${baseUrl}/keys/${platform}/${keyId}?auth_token=${args.token}`, { formData: { data: JSON.stringify(password) } })
+    console.log('Unlocked key')
   }
   // Submit
-  await request.put(`${baseUrl}/apps/${args.appId}?auth_token=${args.token}`, { formData: { file: zippedApp } });
-  console.log('Uploaded source code');
+  await request.put(`${baseUrl}/apps/${args.appId}?auth_token=${args.token}`, { formData: { file: zippedApp } })
+  console.log('Uploaded source code')
   // Start build
-  await request.post(`${baseUrl}/apps/${args.appId}/build/${platform}?auth_token=${args.token}`);
-  console.log('Started build');
+  await request.post(`${baseUrl}/apps/${args.appId}/build/${platform}?auth_token=${args.token}`)
+  console.log('Started build')
   // Wait / Poll
-  let status = 'pending';
+  let status = 'pending'
   while (status === 'pending') {
-    await sleep(pollTime);
-    const e = await request.get(`${baseUrl}/apps/${args.appId}?auth_token=${args.token}`);
-    const result = JSON.parse(e);
-    status = result.status[platform];
-    console.log(`Status: ${status}...`);
+    await sleep(pollTime)
+    const e = await request.get(`${baseUrl}/apps/${args.appId}?auth_token=${args.token}`)
+    const result = JSON.parse(e)
+    status = result.status[platform]
+    console.log(`Status: ${status}...`)
     if (status === 'error') {
-      console.log(`Error: ${result.error[platform]}`);
+      console.log(`Error: ${result.error[platform]}`)
     }
   }
   // Download
-  if (status == 'complete') {
-    const outfilename = `watercoolr-${platform}.${platform  === 'ios' ? 'ipa' : 'apk'}`;
-    const file = await request.get(`${baseUrl}/apps/${args.appId}/${platform}?auth_token=${args.token}`).pipe(fs.createWriteStream(outfilename));
-    console.log(`Downloaded ${outfilename}`);
+  if (status === 'complete') {
+    const outfilename = `watercoolr-${platform}.${platform  === 'ios' ? 'ipa' : 'apk'}`
+    const file = await request.get(`${baseUrl}/apps/${args.appId}/${platform}?auth_token=${args.token}`).pipe(fs.createWriteStream(outfilename))
+    console.log(`Downloaded ${outfilename}`)
   }
 }
 
-build(args.platform);
+build(args.platform)
